7d1292ecccf81cf82f16997c9cd1e58e88a2fa55
arrentrik2-alt <arrentrik2@gmail.com>
2025-08-21 10:49:50 +0200

    Create supabaseClient.js

---
+++ supabaseClient.js
@@ -0,0 +1,4 @@
+// supabaseClient.js
+const supabaseUrl = "https://xsowdwnpguftmlvsogdb.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhzb3dkd25wZ3VmdG1sdnNvZ2RiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU3NTc0MzQsImV4cCI6MjA3MTMzMzQzNH0.yFz01fJCqTuaY-jzgqlcgueiU4Id71t66xtjhAGXNWI";
+const supabase = supabase.createClient(supabaseUrl, supabaseKey);
