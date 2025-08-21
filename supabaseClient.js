48769f5105eb31166e763396ac45f1c5e45243b8
arrentrik2-alt <arrentrik2@gmail.com>
2025-08-21 11:16:36 +0200

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,4 +1,4 @@
 // supabaseClient.js
 var supabaseUrl = "https://xsowdwnpguftmlvsogdb.supabase.co";
 var supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhzb3dkd25wZ3VmdG1sdnNvZ2RiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU3NTc0MzQsImV4cCI6MjA3MTMzMzQzNH0.yFz01fJCqTuaY-jzgqlcgueiU4Id71t66xtjhAGXNWI";
-var supabase = supabase.createClient(supabaseUrl, supabaseKey);
+var supabase = Supabase.createClient(supabaseUrl, supabaseKey);
